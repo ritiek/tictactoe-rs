e9a7f169e9e7247ef46b1df96474a4cb8317e706
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-09-09 17:12:07 +0530

    Convert grid coords to screen coords

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
+use std::fmt;
 use std::io;
 use std::io::{stdout, Write};
-use std::fmt;
 
 use crossterm::{
-    execute,
+    cursor, event, execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand,
-    cursor,
-    event,
-    terminal,
+    terminal, ExecutableCommand,
 };
 
 // trait Error: fmt::Debug + fmt::Display {}
@@ -57,11 +54,23 @@ impl Grid {
 
     fn draw(&mut self) -> crossterm::Result<&mut Self> {
         let Side(side) = self.side;
+        let grid_length = side * 4 - 1;
+        let grid_background = std::iter::repeat(" ")
+            .take(grid_length.into())
+            .collect::<String>();
         for y in 0..side {
-            for x in 0..(side-1) {
+            execute!(
+                stdout(),
+                cursor::MoveTo(0, y),
+                SetBackgroundColor(Color::White),
+                Print(&grid_background),
+                ResetColor
+            )?;
+            for x in 0..(side - 1) {
+                let boundary_position = x * 4 + 3;
                 execute!(
                     stdout(),
-                    cursor::MoveTo((x*4)+3, y),
+                    cursor::MoveTo(boundary_position, y),
                     SetForegroundColor(Color::Black),
                     SetBackgroundColor(Color::White),
                     Print("|"),
@@ -69,32 +78,41 @@ impl Grid {
                 )?;
             }
         }
-        cursor::MoveTo(0, 3);
         Ok(self)
     }
 
-    fn mark_cross(mut self, position: Coordinates) -> io::Result<Self> {
-    // fn mark_cross(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
-        self.mark(position, 'X')
+    fn mark_cross_at(mut self, position: Coordinates) -> io::Result<Self> {
+        // fn mark_cross(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
+        self.mark_at(position, 'X')
     }
 
-    fn mark_zero(mut self, position: Coordinates) -> io::Result<Self> {
-    // fn mark_zero(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
-        self.mark(position, '0')
+    fn mark_zero_at(mut self, position: Coordinates) -> io::Result<Self> {
+        // fn mark_zero(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
+        self.mark_at(position, '0')
     }
 
-    fn mark(mut self, position: Coordinates, marker: char) -> io::Result<Self> {
-    // fn mark(mut self, position: Coordinates, marker: char) -> Result<Self, Box<dyn Error>> {
+    fn mark_at(mut self, position: Coordinates, marker: char) -> io::Result<Self> {
+        // fn mark(mut self, position: Coordinates, marker: char) -> Result<Self, Box<dyn Error>> {
         let Side(side) = &self.side;
         let position = {
             if side >= &position.x && side >= &position.y {
                 Ok(position)
             } else {
-                Err(io::Error::new(io::ErrorKind::Other, "position coordinates are out of bounds"))
+                Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "position coordinates are out of bounds",
+                ))
             }
         }?;
         self.marked_positions.push(position);
         Ok(self)
+    }
+
+    fn grid_coords_to_screen_coords(position: Coordinates) -> Coordinates {
+        Coordinates {
+            x: position.x * 4 + 2,
+            y: position.y,
+        }
     }
 }
 
