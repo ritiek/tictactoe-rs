3e59f9c95de94821f92105ee730f229459dd0624
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-09-13 14:05:27 +0530

    Show player markers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use std::fmt;
 use std::io;
 use std::io::{stdout, Write};
+use std::ops;
+use std::collections::HashMap;
 // use std::{thread, time};
 
 use crossterm::event::{read, Event};
@@ -17,10 +19,21 @@ use crossterm::{cursor, event, execute, terminal, ExecutableCommand};
 #[derive(Debug)]
 struct Side(u16);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Coordinates {
     x: i16,
     y: i16,
+}
+
+impl ops::Add<Coordinates> for Coordinates {
+    type Output = Coordinates;
+
+    fn add(self, rhs: Coordinates) -> Coordinates {
+        Coordinates {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
 
 //0123456789
@@ -47,6 +60,15 @@ enum Player {
     Cross,
 }
 
+impl Player {
+    fn to_char(&self) -> char {
+        match self {
+            Self::Zero => '0',
+            Self::Cross => 'X',
+        }
+    }
+}
+
 #[derive(Debug)]
 // Marked this as non-exhaustive because it's possible to have variants for diagonal
 // movements.
@@ -69,8 +91,10 @@ enum InputEvent {
 }
 
 struct TicTacToe {
+    cursor: Coordinates,
     grid: Grid,
-    marked_positions: Vec<Coordinates>,
+    // marked_positions: Vec<Coordinates>,
+    marked_positions: HashMap<Coordinates, Option<Player>>,
 }
 
 impl Grid {
@@ -114,11 +138,35 @@ impl Grid {
             y: position.y,
         }
     }
+
+    fn mark_at(&mut self, position: Coordinates, marker: char) -> io::Result<&Self> {
+        // fn mark(mut self, position: Coordinates, marker: char) -> Result<Self, Box<dyn Error>> {
+        let Side(side) = &self.side;
+        let position = {
+            if side >= &(position.x as u16) && side >= &(position.y as u16) {
+                Ok(position)
+            } else {
+                Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "position coordinates are out of bounds from the grid area",
+                ))
+            }
+        }?;
+        // Self::move_cursor_to_grid(&position);
+        execute!(
+            stdout(),
+            SetForegroundColor(Color::Red),
+            SetBackgroundColor(Color::White),
+            Print(marker),
+            ResetColor
+        )?;
+        Ok(self)
+    }
 }
 
 impl Default for Grid {
     fn default() -> Self {
-        Self { side: Side(3) }
+        Self { side: Side(7) }
     }
 }
 
@@ -131,10 +179,10 @@ impl Default for Grid {
 // impl Error for Grid {}
 
 impl Direction {
-    fn get_relative_coordinates(&self) -> Coordinates {
+    fn get_relative_coords(&self) -> Coordinates {
         match &self {
-            Direction::Up => Coordinates { x: 0, y: 1 },
-            Direction::Down => Coordinates { x: 0, y: -1 },
+            Direction::Up => Coordinates { x: 0, y: -1 },
+            Direction::Down => Coordinates { x: 0, y: 1 },
             Direction::Left => Coordinates { x: -1, y: 0 },
             Direction::Right => Coordinates { x: 1, y: 0 },
             _ => panic!("Diagonal movement is not yet implemented!"),
@@ -144,72 +192,111 @@ impl Direction {
 
 impl TicTacToe {
     fn from(grid: Grid) -> Self {
+        let initial_grid_coords = Coordinates { x: 0, y: 0};
+        Self::move_cursor_to_grid(&initial_grid_coords);
+        let Side(side) = grid.side;
+        let mut marked_positions: HashMap<Coordinates, Option<Player>> = HashMap::new();
+        marked_positions.reserve(side.pow(2).into());
+        for x in 0..side as i16 {
+            for y in 0..side as i16 {
+                marked_positions.insert(Coordinates { x, y }, None);
+            }
+        }
         Self {
+            cursor: initial_grid_coords,
             grid: grid,
-            marked_positions: vec![],
+            marked_positions: marked_positions,
         }
     }
 
-    fn handle_player_input(&mut self) -> crossterm::Result<()> {
+    fn handle_keyboard_input(&mut self) -> crossterm::Result<()> {
+        let mut event: InputEvent;
         loop {
-            if let Event::Key(k) = read()? {
-                let key = match k.code {
-                    event::KeyCode::Enter => Some(InputEvent::Mark),
-                    event::KeyCode::Char('w') => Some(InputEvent::Direction(Direction::Up)),
-                    event::KeyCode::Char('s') => Some(InputEvent::Direction(Direction::Down)),
-                    event::KeyCode::Char('a') => Some(InputEvent::Direction(Direction::Left)),
-                    event::KeyCode::Char('d') => Some(InputEvent::Direction(Direction::Right)),
-                    event::KeyCode::Esc => Some(InputEvent::Quit),
-                    _ => None,
-                };
-            };
+            event = self.read_input_event()?;
+            match event {
+                InputEvent::Direction(direction) => {
+                    let Side(side) = &self.grid.side;
+                    let mut grid_coords = self.cursor + direction.get_relative_coords();
+                    if grid_coords.x >= *side as i16 {
+                        grid_coords.x = *side as i16 - 1;
+                    }
+                    if grid_coords.y >= *side as i16 {
+                        grid_coords.y = *side as i16 - 1;
+                    }
+                    if grid_coords.x < 0 {
+                        grid_coords.x = 0;
+                    }
+                    if grid_coords.y < 0 {
+                        grid_coords.y = 0;
+                    }
+                    Self::move_cursor_to_grid(&grid_coords);
+                    self.cursor = grid_coords;
+                },
+                InputEvent::Mark => {
+                    self.mark_cross()?;
+                    // The cursor automatically increments in x-axis after placing the mark.
+                    // Let's decrement the cursor back to bring back to its original position.
+                    Self::move_cursor_to_grid(&self.cursor);
+                },
+                InputEvent::Quit => {
+                    break;
+                },
+            }
         }
         Ok(())
     }
 
-    fn mark_cross_at(mut self, position: Coordinates) -> io::Result<Self> {
-        // fn mark_cross(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
-        self.mark_at(position, 'X')
+    fn read_input_event(&self) -> crossterm::Result<InputEvent> {
+        loop {
+            if let Event::Key(k) = read()? {
+                match k.code {
+                    event::KeyCode::Enter => return Ok(InputEvent::Mark),
+                    event::KeyCode::Char('w') => return Ok(InputEvent::Direction(Direction::Up)),
+                    event::KeyCode::Char('s') => return Ok(InputEvent::Direction(Direction::Down)),
+                    event::KeyCode::Char('a') => return Ok(InputEvent::Direction(Direction::Left)),
+                    event::KeyCode::Char('d') => return Ok(InputEvent::Direction(Direction::Right)),
+                    event::KeyCode::Esc => return Ok(InputEvent::Quit),
+                    _ => {},
+                };
+            };
+        }
     }
 
-    fn mark_zero_at(mut self, position: Coordinates) -> io::Result<Self> {
-        // fn mark_zero(mut self, position: Coordinates) -> Result<Self, Box<dyn Error>> {
-        self.mark_at(position, '0')
+    fn handle_input_event(&mut self, event: InputEvent) {
+            // execute!(
+            //     stdout(),
+            //     cursor::MoveTo( as u16, as u16),
+            //     SetForegroundColor(Color::Red),
+            //     SetBackgroundColor(Color::White),
+            //     Print(marker),
+            //     ResetColor
+            // )?;
     }
 
-    fn mark_at(mut self, position: Coordinates, marker: char) -> io::Result<Self> {
-        // fn mark(mut self, position: Coordinates, marker: char) -> Result<Self, Box<dyn Error>> {
-        let Side(side) = &self.grid.side;
-        let position = {
-            if side >= &(position.x as u16) && side >= &(position.y as u16) {
-                Ok(position)
-            } else {
-                Err(io::Error::new(
-                    io::ErrorKind::Other,
-                    "position coordinates are out of bounds from the grid area",
-                ))
-            }
-        }?;
-        let screen_coords = Grid::grid_coords_to_screen_coords(&position);
-        self.marked_positions.push(position);
+    fn move_cursor_to_grid(position: &Coordinates) -> crossterm::Result<()> {
+        let screen_coords = Grid::grid_coords_to_screen_coords(position);
+        Ok(Self::move_cursor_to_screen(&screen_coords)?)
+    }
+
+    fn move_cursor_to_screen(position: &Coordinates) -> crossterm::Result<()> {
         execute!(
             stdout(),
-            cursor::MoveTo(screen_coords.x as u16, screen_coords.y as u16),
-            SetForegroundColor(Color::Red),
-            SetBackgroundColor(Color::White),
-            Print(marker),
-            ResetColor
+            cursor::MoveTo(position.x as u16, position.y as u16)
         )?;
-        // FIXME: below is for testing only
-        execute!(
-            stdout(),
-            cursor::MoveTo(10, 10),
-            SetForegroundColor(Color::Red),
-            SetBackgroundColor(Color::White),
-            Print(marker),
-            ResetColor
-        )?;
+        Ok(())
+    }
+
+    fn mark(&mut self, player: Player) -> io::Result<&Self> {
+        self.grid.mark_at(self.cursor, player.to_char())?;
         Ok(self)
+    }
+
+    fn mark_cross(&mut self) -> io::Result<&Self> {
+        Ok(self.mark(Player::Cross)?)
+    }
+
+    fn mark_zero(&mut self) -> io::Result<&Self> {
+        Ok(self.mark(Player::Zero)?)
     }
 
     fn check_for_victory() -> Option<Player> {
@@ -219,12 +306,13 @@ impl TicTacToe {
 }
 
 fn main() -> crossterm::Result<()> {
+    terminal::enable_raw_mode()?;
     let mut grid: Grid = Default::default();
     grid.draw();
     // let two_s = time::Duration::from_secs(2);
     // thread::sleep(two_s);
     // grid.mark_cross_at(Coordinates { x: 1, y: 0 });
     let mut game = TicTacToe::from(grid);
-    game.handle_player_input();
+    game.handle_keyboard_input();
     Ok(())
 }
